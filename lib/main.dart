701e7b28d9d2ef5f52cae897997a62bcff996730
Grzegorz Fraczek <gf@lnasoftware.com>
2023-05-08 20:04:00 +0200

    Dev test

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // lest add dev test
   @override
   Widget build(BuildContext context) {
     return Scaffold(
